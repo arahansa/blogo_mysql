be4bdefa80c470f32031f510e807d54ef2f3d7aa
arahansa <arahansa@naver.com>
2016-03-05 02:09:46 +0900

    add regex router , sub pattern

--- config/MVCRequestMappingConfig.go
+++ config/MVCRequestMappingConfig.go
@@ -8,9 +8,12 @@ import (
 func InitMVCRequestConfig(){
 	var article = controller.Article{}
 	// mux := http.NewServeMux()
-	http.HandleFunc("/muxTest", article.Article)
-	http.HandleFunc("/", article.Article)
+	http.HandleFunc("/muxTest/", article.Article)
+
 	http.HandleFunc("/article", article.Article)
 	http.ListenAndServe(":80", nil)
 
 }
+
+
+// -- / 로 끝나는 경로는 서브 패턴 허용
--- controller/ArticleController.go
+++ controller/ArticleController.go
@@ -4,13 +4,25 @@ package controller
 import (
 	"net/http"
 	"log"
+	"regexp"
+	"strings"
 )
 
-
+var rNum = regexp.MustCompile(`\d`)  // Has digit(s)
+var rAbc = regexp.MustCompile(`abc`) // Contains "abc"
 
 type Article struct{}
 
 func (a *Article) Article(w http.ResponseWriter, r *http.Request){
+	values := strings.Split(r.URL.Path, "/")
+	log.Println("경로들 : ", values, len(values))
+	
+	switch  {
+	case rNum.MatchString(r.URL.Path):
+		log.Println("숫자만 있네요?" , r.URL.Path)
+	case rAbc.MatchString(r.URL.Path) :
+		log.Println("abc를 포함하네요 ? ", r.URL.Path)
+	}
 	log.Println(" r :: ", r)
 	log.Println(" Method ::"+r.Method)
 	log.Println(" Path :: " , r.RequestURI)
